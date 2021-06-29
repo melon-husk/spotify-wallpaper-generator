f9fe626b18b552d439ac354417d4af4bff91bdb9
melon-husk <ujwal5555@gmail.com>
2021-06-29 18:10:40 +0530

    added more functions

--- src/helpers/shuffleArray.ts
+++ src/helpers/shuffleArray.ts
@@ -1,20 +1,25 @@
+import showError from "./showError";
+
 function shuffle(array: Array<any>): Array<any> {
-  var currentIndex = array.length,
-    randomIndex;
+  try {
+    var currentIndex = array.length,
+      randomIndex;
 
-  // While there remain elements to shuffle...
-  while (0 !== currentIndex) {
-    // Pick a remaining element...
-    randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex--;
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
 
-    // And swap it with the current element.
-    [array[currentIndex], array[randomIndex]] = [
-      array[randomIndex],
-      array[currentIndex],
-    ];
+      // And swap it with the current element.
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex],
+        array[currentIndex],
+      ];
+    }
+  } catch (error: any) {
+    showError(error);
   }
-
   return array;
 }
 
